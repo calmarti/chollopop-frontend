3a631a62e6a7d9be012c3d43d25c2a0fe16f234e
Ivan Martinez <ivmartinezc@gmail.com>
2022-11-01 13:27:13 +0100

    acabado del logo con pseudo-elements

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,7 +3,7 @@ import './LandingPage.css';
 export default function LandingPage() {
   return (
     <div className="mt-5 mx-5">
-      <h1 className="chollopop-title mb-3 mx-3">Chollopop</h1>
+      <h1 className="chollopop-title mb-3 mx-3">Chollopop<span className="logo-icon"></span></h1>
       <p className="chollopop-description">La web de los que encuentran lo que buscan</p>
       <p className="chollopop-question">¿Que necesitas hoy?</p>
 
